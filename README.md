aa1ae92c768b2ba020e7a513dff8e71da6ec2405
frolovae <evgeniya.o.frolova@gmail.com>
2015-10-13 00:51:36 +0200

    tests implemented

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
+APPLICATION MANUAL
+1 - save folder frontend-nanodegree-feedreader on a disk;
+2 - open index.html in a broser.
+
+
 # Project Overview
 
 In this project you are given a web-based application that reads RSS feeds. The original developer of this application clearly saw the value in testing, they've already included [Jasmine](http://jasmine.github.io/) and even started writing their first test suite! Unfortunately, they decided to move on to start their own company and we're now left with an application with an incomplete test suite. That's where you come in.
--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,28 +31,58 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
+         it('all urls are defind', function() {
+            allFeeds.forEach(function(feed,i) {
+                expect(feed.url).toBeDefined();
+                expect(feed.url).not.toBe('');
+            });
+         });
 
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+
+         it('all names are defind', function() {
+            allFeeds.forEach(function(feed,i) {
+                expect(feed.name).toBeDefined();
+                expect(feed.name).not.toBe('');
+            });
+         });
     });
 
-
+    describe('The menu', function() {
     /* TODO: Write a new test suite named "The menu" */
-
+        var bodyCSSClass = document.body.className;
+        var menuIcon = $('.menu-icon-link');
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
 
+        it('by default menu is hidden', function() {
+            expect(bodyCSSClass).toBe('menu-hidden');
+        });
+
          /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
+
+        it('menu changes visibility when menu icon is clicked', function() {
+            menuIcon.click();
+            expect(document.body.className).not.toBe('menu-hidden');
+            menuIcon.click();
+            expect(document.body.className).toBe('menu-hidden');
+                      
+        });
+
+    });
+
+    describe('Initial Entries', function() {
 
     /* TODO: Write a new test suite named "Initial Entries" */
 
@@ -63,10 +93,48 @@ $(function() {
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
 
+        beforeEach(function(done) {
+            setTimeout(function() {
+                loadFeed(0, function() {
+                    done();
+                });
+            }, 1);
+        });
+        
+        it('there is at list a single entry within feed container', function(done) {
+            var container = $('.feed');
+            expect(container.children().length).not.toBe(0);
+            done();
+        });
+    });
+
     /* TODO: Write a new test suite named "New Feed Selection"
 
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+
+    describe('New Feed Selection', function(){
+        var currentValue;
+        var newValue;
+
+        beforeEach(function(done) {
+            setTimeout(function() {
+                loadFeed(0, function() {
+                    currentValue = $('.feed').html();
+                    loadFeed(1, function() {
+                        newValue = $('.feed').html();
+                        done();    
+                    }); 
+                });
+                
+            }, 1);
+        });
+
+        it('each time when loadFeed runs feed content changes', function(done) { 
+            expect(newValue).not.toBe(currentValue);
+            done();
+        });
+    });
 }());
